1f5c5c2dc68e10b09d28d7595526d0c9aa66a61c
underwatergrasshopper <underwater.grasshopper@gmail.com>
2023-09-21 19:29:08 +0200

    Cleaned code.

--- src/poe_exp_after_dot/_Private/Overlay.py
+++ src/poe_exp_after_dot/_Private/Overlay.py
@@ -3,11 +3,6 @@ from time import time as _get_time
 from typing import SupportsFloat, SupportsInt, Sequence
 from dataclasses import dataclass
 
-from PySide6.QtWidgets import QMainWindow, QApplication, QWidget, QLabel, QSystemTrayIcon, QMenu
-from PySide6.QtCore import Qt, QPoint, QRect
-from PySide6.QtGui import QColor, QMouseEvent, QIcon, QAction, QCloseEvent
-from PySide6.QtCore import QPoint, QRect
-
 import os
 import re
 import numpy
@@ -17,10 +12,124 @@ import ctypes
 
 from PIL import ImageGrab
 
+from PySide6.QtWidgets import QMainWindow, QApplication, QWidget, QLabel, QSystemTrayIcon, QMenu
+from PySide6.QtCore import Qt, QPoint, QRect
+from PySide6.QtGui import QColor, QMouseEvent, QIcon, QAction, QCloseEvent
+from PySide6.QtCore import QPoint, QRect
+
 _SECONDS_IN_MINUTE   = 60
 _SECONDS_IN_HOUR     = 60 * _SECONDS_IN_MINUTE
 _SECONDS_IN_DAY      = 24 * _SECONDS_IN_HOUR
 _SECONDS_IN_WEEK     = 7  * _SECONDS_IN_DAY
+
+@dataclass
+class ExpThresholdInfo:
+    level       : int
+    base_exp    : int
+    exp_to_next : int
+
+_EXP_THRESHOLD_INFO_TABLE = (
+    ExpThresholdInfo(1, 0, 525),
+    ExpThresholdInfo(2, 525, 1235),
+    ExpThresholdInfo(3, 1760, 2021),
+    ExpThresholdInfo(4, 3781, 3403),
+    ExpThresholdInfo(5, 7184, 5002),
+    ExpThresholdInfo(6, 12186, 7138),
+    ExpThresholdInfo(7, 19324, 10053),
+    ExpThresholdInfo(8, 29377, 13804),
+    ExpThresholdInfo(9, 43181, 18512),
+    ExpThresholdInfo(10, 61693, 24297),
+    ExpThresholdInfo(11, 85990, 31516),
+    ExpThresholdInfo(12, 117506, 39878),
+    ExpThresholdInfo(13, 157384, 50352),
+    ExpThresholdInfo(14, 207736, 62261),
+    ExpThresholdInfo(15, 269997, 76465),
+    ExpThresholdInfo(16, 346462, 92806),
+    ExpThresholdInfo(17, 439268, 112027),
+    ExpThresholdInfo(18, 551295, 133876),
+    ExpThresholdInfo(19, 685171, 158538),
+    ExpThresholdInfo(20, 843709, 187025),
+    ExpThresholdInfo(21, 1030734, 218895),
+    ExpThresholdInfo(22, 1249629, 255366),
+    ExpThresholdInfo(23, 1504995, 295852),
+    ExpThresholdInfo(24, 1800847, 341805),
+    ExpThresholdInfo(25, 2142652, 392470),
+    ExpThresholdInfo(26, 2535122, 449555),
+    ExpThresholdInfo(27, 2984677, 512121),
+    ExpThresholdInfo(28, 3496798, 583857),
+    ExpThresholdInfo(29, 4080655, 662181),
+    ExpThresholdInfo(30, 4742836, 747411),
+    ExpThresholdInfo(31, 5490247, 844146),
+    ExpThresholdInfo(32, 6334393, 949053),
+    ExpThresholdInfo(33, 7283446, 1100952),
+    ExpThresholdInfo(34, 8384398, 1156712),
+    ExpThresholdInfo(35, 9541110, 1333241),
+    ExpThresholdInfo(36, 10874351, 1487491),
+    ExpThresholdInfo(37, 12361842, 1656447),
+    ExpThresholdInfo(38, 14018289, 1841143),
+    ExpThresholdInfo(39, 15859432, 2046202),
+    ExpThresholdInfo(40, 17905634, 2265837),
+    ExpThresholdInfo(41, 20171471, 2508528),
+    ExpThresholdInfo(42, 22679999, 2776124),
+    ExpThresholdInfo(43, 25456123, 3061734),
+    ExpThresholdInfo(44, 28517857, 3379914),
+    ExpThresholdInfo(45, 31897771, 3723676),
+    ExpThresholdInfo(46, 35621447, 4099570),
+    ExpThresholdInfo(47, 39721017, 4504444),
+    ExpThresholdInfo(48, 44225461, 4951099),
+    ExpThresholdInfo(49, 49176560, 5430907),
+    ExpThresholdInfo(50, 54607467, 5957868),
+    ExpThresholdInfo(51, 60565335, 6528910),
+    ExpThresholdInfo(52, 67094245, 7153414),
+    ExpThresholdInfo(53, 74247659, 7827968),
+    ExpThresholdInfo(54, 82075627, 8555414),
+    ExpThresholdInfo(55, 90631041, 9353933),
+    ExpThresholdInfo(56, 99984974, 10212541),
+    ExpThresholdInfo(57, 110197515, 11142646),
+    ExpThresholdInfo(58, 121340161, 12157041),
+    ExpThresholdInfo(59, 133497202, 13252160),
+    ExpThresholdInfo(60, 146749362, 14441758),
+    ExpThresholdInfo(61, 161191120, 15731508),
+    ExpThresholdInfo(62, 176922628, 17127265),
+    ExpThresholdInfo(63, 194049893, 18635053),
+    ExpThresholdInfo(64, 212684946, 20271765),
+    ExpThresholdInfo(65, 232956711, 22044909),
+    ExpThresholdInfo(66, 255001620, 23950783),
+    ExpThresholdInfo(67, 278952403, 26019833),
+    ExpThresholdInfo(68, 304972236, 28261412),
+    ExpThresholdInfo(69, 333233648, 30672515),
+    ExpThresholdInfo(70, 363906163, 33287878),
+    ExpThresholdInfo(71, 397194041, 36118904),
+    ExpThresholdInfo(72, 433312945, 39163425),
+    ExpThresholdInfo(73, 472476370, 42460810),
+    ExpThresholdInfo(74, 514937180, 46024718),
+    ExpThresholdInfo(75, 560961898, 49853964),
+    ExpThresholdInfo(76, 610815862, 54008554),
+    ExpThresholdInfo(77, 664824416, 58473753),
+    ExpThresholdInfo(78, 723298169, 63314495),
+    ExpThresholdInfo(79, 786612664, 68516464),
+    ExpThresholdInfo(80, 855129128, 74132190),
+    ExpThresholdInfo(81, 929261318, 80182477),
+    ExpThresholdInfo(82, 1009443795, 86725730),
+    ExpThresholdInfo(83, 1096169525, 93748717),
+    ExpThresholdInfo(84, 1189918242, 101352108),
+    ExpThresholdInfo(85, 1291270350, 109524907),
+    ExpThresholdInfo(86, 1400795257, 118335069),
+    ExpThresholdInfo(87, 1519130326, 127813148),
+    ExpThresholdInfo(88, 1646943474, 138033822),
+    ExpThresholdInfo(89, 1784977296, 149032391),
+    ExpThresholdInfo(90, 1934009687, 160890604),
+    ExpThresholdInfo(91, 2094900291, 173648795),
+    ExpThresholdInfo(92, 2268549086, 187372170),
+    ExpThresholdInfo(93, 2455921256, 202153736),
+    ExpThresholdInfo(94, 2658074992, 218041909),
+    ExpThresholdInfo(95, 2876116901, 235163399),
+    ExpThresholdInfo(96, 3111280300, 253547862),
+    ExpThresholdInfo(97, 3364828162, 273358532),
+    ExpThresholdInfo(98, 3638186694, 294631836),
+    ExpThresholdInfo(99, 3932818530, 317515914),
+    ExpThresholdInfo(100, 4250334444, 0),
+)
 
 def _pad_to_length(text : str, length : int):
         """
@@ -329,6 +438,8 @@ class FineExpPerHour:
     
 class FinePercent:
     MAX_LENGTH_AFTER_FORMAT : int       = 9
+    # >+100.00%
+    # <-100.00%
 
     _percent                : float
     _integer                : int       # in percent
@@ -402,12 +513,6 @@ class FinePercent:
     
     def __repr__(self) -> str:
         return self._text_representation
-    
-@dataclass
-class ExpThresholdInfo:
-    level       : int
-    base_exp    : int
-    exp_to_next : int
 
 class StopWatch:
     _start          : float     # in seconds
@@ -603,108 +708,7 @@ def _find_exp_threshold_info(exp : int) -> ExpThresholdInfo | None:
             return info 
     return None
 
-_EXP_THRESHOLD_INFO_TABLE = (
-    ExpThresholdInfo(1, 0, 525),
-    ExpThresholdInfo(2, 525, 1235),
-    ExpThresholdInfo(3, 1760, 2021),
-    ExpThresholdInfo(4, 3781, 3403),
-    ExpThresholdInfo(5, 7184, 5002),
-    ExpThresholdInfo(6, 12186, 7138),
-    ExpThresholdInfo(7, 19324, 10053),
-    ExpThresholdInfo(8, 29377, 13804),
-    ExpThresholdInfo(9, 43181, 18512),
-    ExpThresholdInfo(10, 61693, 24297),
-    ExpThresholdInfo(11, 85990, 31516),
-    ExpThresholdInfo(12, 117506, 39878),
-    ExpThresholdInfo(13, 157384, 50352),
-    ExpThresholdInfo(14, 207736, 62261),
-    ExpThresholdInfo(15, 269997, 76465),
-    ExpThresholdInfo(16, 346462, 92806),
-    ExpThresholdInfo(17, 439268, 112027),
-    ExpThresholdInfo(18, 551295, 133876),
-    ExpThresholdInfo(19, 685171, 158538),
-    ExpThresholdInfo(20, 843709, 187025),
-    ExpThresholdInfo(21, 1030734, 218895),
-    ExpThresholdInfo(22, 1249629, 255366),
-    ExpThresholdInfo(23, 1504995, 295852),
-    ExpThresholdInfo(24, 1800847, 341805),
-    ExpThresholdInfo(25, 2142652, 392470),
-    ExpThresholdInfo(26, 2535122, 449555),
-    ExpThresholdInfo(27, 2984677, 512121),
-    ExpThresholdInfo(28, 3496798, 583857),
-    ExpThresholdInfo(29, 4080655, 662181),
-    ExpThresholdInfo(30, 4742836, 747411),
-    ExpThresholdInfo(31, 5490247, 844146),
-    ExpThresholdInfo(32, 6334393, 949053),
-    ExpThresholdInfo(33, 7283446, 1100952),
-    ExpThresholdInfo(34, 8384398, 1156712),
-    ExpThresholdInfo(35, 9541110, 1333241),
-    ExpThresholdInfo(36, 10874351, 1487491),
-    ExpThresholdInfo(37, 12361842, 1656447),
-    ExpThresholdInfo(38, 14018289, 1841143),
-    ExpThresholdInfo(39, 15859432, 2046202),
-    ExpThresholdInfo(40, 17905634, 2265837),
-    ExpThresholdInfo(41, 20171471, 2508528),
-    ExpThresholdInfo(42, 22679999, 2776124),
-    ExpThresholdInfo(43, 25456123, 3061734),
-    ExpThresholdInfo(44, 28517857, 3379914),
-    ExpThresholdInfo(45, 31897771, 3723676),
-    ExpThresholdInfo(46, 35621447, 4099570),
-    ExpThresholdInfo(47, 39721017, 4504444),
-    ExpThresholdInfo(48, 44225461, 4951099),
-    ExpThresholdInfo(49, 49176560, 5430907),
-    ExpThresholdInfo(50, 54607467, 5957868),
-    ExpThresholdInfo(51, 60565335, 6528910),
-    ExpThresholdInfo(52, 67094245, 7153414),
-    ExpThresholdInfo(53, 74247659, 7827968),
-    ExpThresholdInfo(54, 82075627, 8555414),
-    ExpThresholdInfo(55, 90631041, 9353933),
-    ExpThresholdInfo(56, 99984974, 10212541),
-    ExpThresholdInfo(57, 110197515, 11142646),
-    ExpThresholdInfo(58, 121340161, 12157041),
-    ExpThresholdInfo(59, 133497202, 13252160),
-    ExpThresholdInfo(60, 146749362, 14441758),
-    ExpThresholdInfo(61, 161191120, 15731508),
-    ExpThresholdInfo(62, 176922628, 17127265),
-    ExpThresholdInfo(63, 194049893, 18635053),
-    ExpThresholdInfo(64, 212684946, 20271765),
-    ExpThresholdInfo(65, 232956711, 22044909),
-    ExpThresholdInfo(66, 255001620, 23950783),
-    ExpThresholdInfo(67, 278952403, 26019833),
-    ExpThresholdInfo(68, 304972236, 28261412),
-    ExpThresholdInfo(69, 333233648, 30672515),
-    ExpThresholdInfo(70, 363906163, 33287878),
-    ExpThresholdInfo(71, 397194041, 36118904),
-    ExpThresholdInfo(72, 433312945, 39163425),
-    ExpThresholdInfo(73, 472476370, 42460810),
-    ExpThresholdInfo(74, 514937180, 46024718),
-    ExpThresholdInfo(75, 560961898, 49853964),
-    ExpThresholdInfo(76, 610815862, 54008554),
-    ExpThresholdInfo(77, 664824416, 58473753),
-    ExpThresholdInfo(78, 723298169, 63314495),
-    ExpThresholdInfo(79, 786612664, 68516464),
-    ExpThresholdInfo(80, 855129128, 74132190),
-    ExpThresholdInfo(81, 929261318, 80182477),
-    ExpThresholdInfo(82, 1009443795, 86725730),
-    ExpThresholdInfo(83, 1096169525, 93748717),
-    ExpThresholdInfo(84, 1189918242, 101352108),
-    ExpThresholdInfo(85, 1291270350, 109524907),
-    ExpThresholdInfo(86, 1400795257, 118335069),
-    ExpThresholdInfo(87, 1519130326, 127813148),
-    ExpThresholdInfo(88, 1646943474, 138033822),
-    ExpThresholdInfo(89, 1784977296, 149032391),
-    ExpThresholdInfo(90, 1934009687, 160890604),
-    ExpThresholdInfo(91, 2094900291, 173648795),
-    ExpThresholdInfo(92, 2268549086, 187372170),
-    ExpThresholdInfo(93, 2455921256, 202153736),
-    ExpThresholdInfo(94, 2658074992, 218041909),
-    ExpThresholdInfo(95, 2876116901, 235163399),
-    ExpThresholdInfo(96, 3111280300, 253547862),
-    ExpThresholdInfo(97, 3364828162, 273358532),
-    ExpThresholdInfo(98, 3638186694, 294631836),
-    ExpThresholdInfo(99, 3932818530, 317515914),
-    ExpThresholdInfo(100, 4250334444, 0),
-)
+
 
 @dataclass
 class PosData:
@@ -951,47 +955,34 @@ class ControlBar(QMainWindow):
         self._measurer.update(total_exp, self._stop_watch.get_elapsed_time())
 
         if self._is_first_measure:
-            self.set_exp_info_board_description(
-                level               = "?" * FineBareLevel.MAX_LENGTH_AFTER_FORMAT,
-                progress            = "?" * FinePercent.MAX_LENGTH_AFTER_FORMAT,
-                progress_step       = "?" * FinePercent.MAX_LENGTH_AFTER_FORMAT,
-                progress_step_time  = "?" * FineTime.MAX_LENGTH_AFTER_FORMAT,
-                exp_per_hour        = "?" * FineExpPerHour.MAX_LENGTH_AFTER_FORMAT,
-                time_to_10_percent  = "?" * FineTime.MAX_LENGTH_AFTER_FORMAT,
-                time_to_next_level  = "?" * FineTime.MAX_LENGTH_AFTER_FORMAT,
-
-                is_resize = True,
-            )
+            self.set_description_in_exp_info_board(is_resize_with_control_description = True)
             self._is_first_measure = False
 
-        self.set_exp_info_board_description(
-            level               = FineBareLevel(self._measurer.get_level()),
-            progress            = FinePercent(self._measurer.get_progress(), integer_color = "#F8CD82", two_dig_after_dot_color = "#7F7FFF"),
-            progress_step       = FinePercent(self._measurer.get_progress_step(), is_sign = True, integer_color = "#FFFF7F", two_dig_after_dot_color = "#FFFF7F"),
-            progress_step_time  = FineTime(self._measurer.get_progress_step_time(), max_unit = "h", unit_color = "#8F8F8F", never_color = "#FF4F1F"),
-            exp_per_hour        = FineExpPerHour(self._measurer.get_exp_per_hour(), value_color = "#6FFF6F", unit_color = "#9F9F9F"),
-            time_to_10_percent  = FineTime(self._measurer.get_time_to_10_percent(), max_unit = "h", unit_color = "#9F9F9F", never_color = "#FF4F1F"),
-            time_to_next_level  = FineTime(self._measurer.get_time_to_next_level(), max_unit = "h", unit_color = "#9F9F9F", never_color = "#FF4F1F")
-        )
+        self.set_description_in_exp_info_board()
 
         ratio = self._measurer.get_progress()
         ratio = ratio - int(ratio)
 
         self._exp_bar.set_area(ratio)
 
-    def set_exp_info_board_description(
-            self, 
-            level               : str | FineBareLevel, 
-            progress            : str | FinePercent, 
-            progress_step       : str | FinePercent,
-            progress_step_time  : str | FineTime,
-            exp_per_hour        : str | FineExpPerHour,
-            time_to_10_percent  : str | FineTime,
-            time_to_next_level  : str | FineTime,
-            *,
-            is_resize = False,
-                ):
-        
+    def set_description_in_exp_info_board(self, is_resize_with_control_description = False):
+        if is_resize_with_control_description:
+            level               = "?" * FineBareLevel.MAX_LENGTH_AFTER_FORMAT
+            progress            = "?" * FinePercent.MAX_LENGTH_AFTER_FORMAT
+            progress_step       = "?" * FinePercent.MAX_LENGTH_AFTER_FORMAT
+            progress_step_time  = "?" * FineTime.MAX_LENGTH_AFTER_FORMAT
+            exp_per_hour        = "?" * FineExpPerHour.MAX_LENGTH_AFTER_FORMAT
+            time_to_10_percent  = "?" * FineTime.MAX_LENGTH_AFTER_FORMAT
+            time_to_next_level  = "?" * FineTime.MAX_LENGTH_AFTER_FORMAT
+        else:
+            level               = FineBareLevel(self._measurer.get_level())
+            progress            = FinePercent(self._measurer.get_progress(), integer_color = "#F8CD82", two_dig_after_dot_color = "#7F7FFF")
+            progress_step       = FinePercent(self._measurer.get_progress_step(), is_sign = True, integer_color = "#FFFF7F", two_dig_after_dot_color = "#FFFF7F")
+            progress_step_time  = FineTime(self._measurer.get_progress_step_time(), max_unit = "h", unit_color = "#8F8F8F", never_color = "#FF4F1F")
+            exp_per_hour        = FineExpPerHour(self._measurer.get_exp_per_hour(), value_color = "#6FFF6F", unit_color = "#9F9F9F")
+            time_to_10_percent  = FineTime(self._measurer.get_time_to_10_percent(), max_unit = "h", unit_color = "#9F9F9F", never_color = "#FF4F1F")
+            time_to_next_level  = FineTime(self._measurer.get_time_to_next_level(), max_unit = "h", unit_color = "#9F9F9F", never_color = "#FF4F1F")
+
         description = (
             f"LVL {level} {progress}<br>"
             f"{progress_step} in {progress_step_time}<br>"
@@ -1000,7 +991,7 @@ class ControlBar(QMainWindow):
             f"next in {time_to_next_level}"
         )
 
-        self._exp_info_board.set_description(description, is_lock_left_bottom = True, is_resize = is_resize)
+        self._exp_info_board.set_description(description, is_lock_left_bottom = True, is_resize = is_resize_with_control_description)
 
     def fetch_exp(self, x : int, y: int) -> int:
         """
