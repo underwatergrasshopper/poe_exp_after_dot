5e0ece9431f9902d55fed95e452df9c3fef34051
underwatergrasshopper <underwater.grasshopper@gmail.com>
2023-09-24 15:25:10 +0200

    Changed handling of exceptions.

--- src/poe_exp_after_dot/__init__.py
+++ src/poe_exp_after_dot/__init__.py
@@ -1,25 +1,42 @@
 
 import logging as _logging
-from ._Private.Overlay import Overlay as _Overlay
+from ._Private.Overlay import Overlay as _Overlay, _EXIT_FAILURE
+
 
 def _main(argv : list[str]) -> int:
-    overlay = _Overlay()
-    return overlay.main(argv)
-
-def _main_try_log_when_exception(argv : list[str]) -> int:
+    """
+    Returns
+        Exit code.
+    """
     try:
-        exit_code = _main(argv)
+        overlay = _Overlay()
+        exit_code = overlay.main(argv)
+
     except Exception as exception:
+        # All internal exceptions are handled here, if logger managed to setup correctly.
         logger = _logging.getLogger("poe_exp_after_dot")
+
         if logger and logger.hasHandlers():
             logger.critical("", exc_info = True)
-            return 1
+            return _EXIT_FAILURE
         else:
             raise exception
     else:
         return exit_code
 
+
+def main(argv : list[str] | None = None) -> int:
+    """
+    Returns
+        Exit code.
+    """
+    if argv is not None and not (isinstance(argv, list) and all([isinstance(option, str) for option in argv])):
+        raise TypeError("Unexpected type of 'argv' parameter.")
+    
+    return _main(argv if argv else [])
+
+
 if __name__ == "__main__":
     import sys as _sys
 
-    _sys.exit(_main_try_log_when_exception(_sys.argv))
+    _sys.exit(_main(_sys.argv))
