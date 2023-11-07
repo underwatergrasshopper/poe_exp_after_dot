79f45a5a077b4b486dc31bfb2e32e3c3014f795a
underwatergrasshopper <underwater.grasshopper@gmail.com>
2023-11-07 19:50:47 +0100

    Changed font for Run64.bat.

--- Run64.bat
+++ Run64.bat
@@ -1,3 +1,3 @@
 @echo off
 set PATH=src;%PATH%
-py -3-64 .\\tests\\run.py --data-path="./out/data" --font="Consolas,14,bold" %*
+py -3-64 .\\tests\\run.py --data-path="./out/data" --font="Consolas,16," %*
