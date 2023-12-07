5edae859b0375bfb5d8d043ab9b5470ce96ad0eb
underwatergrasshopper <underwater.grasshopper@gmail.com>
2023-12-07 17:01:59 +0100

    Changed version.

--- src/poe_exp_after_dot/__init__.py
+++ src/poe_exp_after_dot/__init__.py
@@ -8,7 +8,7 @@ poe_exp_after_dot.py --help
 poe_exp_after_dot.py [<option>...]
 """
 __author__  = "underwatergrasshopper"
-__version__ = "0.1.0rc1"
+__version__ = "0.1.0"
 
 
 import os           as _os
