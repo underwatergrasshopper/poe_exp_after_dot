5052908d7a9532479989be76d334131cea4a0393
underwatergrasshopper <underwater.grasshopper@gmail.com>
2024-03-25 22:42:49 +0100

    Changed build script to build for specific.

--- Build64.bat
+++ Build64.bat
@@ -1,6 +1,7 @@
 :: Build64.bat [<python_version>]
 :: <python_version>
 ::     3                
+::     3.x
 ::     3.11             (default)
 
 @echo off
@@ -9,10 +10,12 @@ setlocal EnableDelayedExpansion
 set PROJECT_PATH=%~dp0
 set PYTHON_VERSION=%1
 
-if "!PYTHON_VERSION!" neq "" (
-    set PY_PYTHON_VERSION=-!PYTHON_VERSION!-64
-) else (
-    set PY_PYTHON_VERSION=-3.11-64
+if "!PYTHON_VERSION!" equ "" (
+    set PYTHON_VERSION=3.11
 )
 
-py !PY_PYTHON_VERSION! "%PROJECT_PATH%setup.py" bdist_wheel --plat-name=win-amd64
+set PYTHON_TAG=!PYTHON_VERSION:.=!
+
+set PY_PYTHON_VERSION=-!PYTHON_VERSION!-64
+
+py !PY_PYTHON_VERSION! "%PROJECT_PATH%setup.py" bdist_wheel --plat-name=win-amd64 --python-tag=py!PYTHON_TAG!
